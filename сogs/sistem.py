b83fa99d93ec1b05d3cbe69a69391ce7f99e61df
Fenek7645 <75085327+Fenek7645@users.noreply.github.com>
2021-10-01 15:03:20 +0300

    8
    Сделал раздачу ролей игровых

--- сogs/sistem.py
+++ сogs/sistem.py
@@ -11,7 +11,13 @@ class sistem(commands.Cog):
         self.bot = bot
         self.cluster = MongoClient("mongodb+srv://Fenek7645:qIFXybPrqBECqqcH@cluster0.aujkm.mongodb.net/ecodb?retryWrites=true&w=majority")
         self.collection = self.cluster.ecodb.colldb
-
+        self.role_message_id = 882310245504942081 # ID of the message that can be reacted to to add/remove a role.
+        self.emoji_to_role = {
+            discord.PartialEmoji(name='⛏'): 880919466463088662,
+            discord.PartialEmoji(name='🕯'): 880895891546968074,
+            discord.PartialEmoji(name='🕹'): 880905281008173097,
+            discord.PartialEmoji(name='🧙'): 882290339891793940,
+        }
     @commands.command(aliases = ['info'] )
     async def инфо(self, ctx, member:discord.Member = None):
         if member == None:
@@ -39,7 +45,83 @@ class sistem(commands.Cog):
             emb.set_thumbnail(url=member.avatar_url)
             emb.set_footer(text = f'{self.bot.user.name} © 2021', icon_url = self.bot.user.avatar_url)
             await ctx.send(embed = emb)
+    
+    @commands.command()
+    @commands.has_role(601771612701851662)
+    async def react2(self, ctx, *, message):
+        await ctx.message.delete()
+        embed_odj = discord.Embed(description=message, color=0x6495ed)
+        message = await ctx.send(embed=embed_odj)
+        await message.add_reaction('⛏')
+        await message.add_reaction('🕯')
+        await message.add_reaction('🕹')
+        await message.add_reaction('🧙')
 
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        """Gives a role based on a reaction emoji."""
+        # Make sure that the message the user is reacting to is the one we care about.
+        if payload.message_id != self.role_message_id:
+            return
+
+        guild = self.bot.get_guild(payload.guild_id)
+        if guild is None:
+            # Check if we're still in the guild and it's cached.
+            return
+
+        try:
+            role_id = self.emoji_to_role[payload.emoji]
+        except KeyError:
+            # If the emoji isn't the one we care about then exit as well.
+            return
+
+        role = guild.get_role(role_id)
+        if role is None:
+            # Make sure the role still exists and is valid.
+            return
+
+        try:
+            # Finally, add the role.
+            await payload.member.add_roles(role)
+        except discord.HTTPException:
+            # If we want to do something in case of errors we'd do it here.
+            pass
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
+        """Removes a role based on a reaction emoji."""
+        # Make sure that the message the user is reacting to is the one we care about.
+        if payload.message_id != self.role_message_id:
+            return
+
+        guild = self.bot.get_guild(payload.guild_id)
+        if guild is None:
+            # Check if we're still in the guild and it's cached.
+            return
+
+        try:
+            role_id = self.emoji_to_role[payload.emoji]
+        except KeyError:
+            # If the emoji isn't the one we care about then exit as well.
+            return
+
+        role = guild.get_role(role_id)
+        if role is None:
+            # Make sure the role still exists and is valid.
+            return
+
+        # The payload for `on_raw_reaction_remove` does not provide `.member`
+        # so we must get the member ourselves from the payload's `.user_id`.
+        member = guild.get_member(payload.user_id)
+        if member is None:
+            # Make sure the member still exists and is valid.
+            return
+
+        try:
+            # Finally, remove the role.
+            await member.remove_roles(role)
+        except discord.HTTPException:
+            # If we want to do something in case of errors we'd do it here.
+            pass
     #latancy
     @commands.command(aliases = ['latancy', 'ping'] )
     async def пинг(self, ctx):
